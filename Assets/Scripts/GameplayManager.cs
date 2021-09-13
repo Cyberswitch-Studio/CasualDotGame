37834d8e1a93da127fe628784c4d6dad58c78544
BartyDash <writetobartek@gmail.com>
2021-09-13 23:06:47 +0200

    DoTween Pro + początkowe skrypty stanu gry

---
+++ Assets/Scripts/GameplayManager.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameplayManager : MonoBehaviour
+{
+
+    public enum GameState
+    {
+        MainMenu,
+        OptionsMenu,
+        PauseMenu,
+        Game
+    }
+
+    public GameState currentState;
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
